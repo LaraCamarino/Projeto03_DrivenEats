df04f0d95bb1dacd9da9cdd1dd8cc04ab2b35ac6
laracamarino <laracamarino@gmail.com>
2022-04-04 07:39:00 -0300

    Versão 3 - Botão de Finalizar Pedidos Implementado

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+
 function selecionarPrato(prato) {
     console.log(prato);
     
@@ -8,7 +9,9 @@ function selecionarPrato(prato) {
     }
 
     prato.classList.add("borda-verde");
+    fecharPedido();
 }
+
 function selecionarBebida(bebida) {
     console.log(bebida);
     
@@ -19,7 +22,9 @@ function selecionarBebida(bebida) {
     }
 
     bebida.classList.add("borda-verde");
+    fecharPedido();
 }
+
 function selecionarSobremesa(sobremesa) {
     console.log(sobremesa);
     
@@ -30,12 +35,49 @@ function selecionarSobremesa(sobremesa) {
     }
 
     sobremesa.classList.add("borda-verde");
+
+    fecharPedido();
 }
 
 function fecharPedido() {
+    const pratoSelecionado = document.querySelector('.borda-verde');
+    const bebidaSelecionado = document.querySelector(".bebidas").querySelector(".borda-verde");
+    const sobremesaSelecionado = document.querySelector(".sobremesas").querySelector(".borda-verde");
+    const botaoPedidoBloqueado = document.querySelector('.botao-cinza');
+    const botaoPedidoLiberado = document.querySelector('.botao-verde');
+
+
+    if ((pratoSelecionado !== null) && (bebidaSelecionado !== null) && (sobremesaSelecionado !== null)) {
+            
+        botaoPedidoBloqueado.classList.add('esconder');
+        botaoPedidoLiberado.classList.add("aparecer");
+               
+    }    
+}
+
+function finalizarPedido() {
+    const nomeCliente = prompt("Qual o seu nome?");
+    const enderecoCliente = prompt("Qual o seu endereço?");
+    const mensagem = "Olá, gostaria de fazer o pedido:
+    - Prato: ${pedido.prato}
+    - Bebida: ${pedido.bebida}
+    - Sobremesa: ${pedido.sobremesa}
+    Total: R$ ${pedido.total}
+    
+    Nome: ${nomeCliente}
+    Endereço: ${enderecoCliente}"";
+
+}
+
+function convesaWhatsApp() {
 
 
     
+    const enderecoSite = encodeURIComponent(mensagem);
+
+    const whatsapp = `https://wa.me/5553111111111?text=${pedidoZap}`;
+    window.open(whatsapp, "_blank")
+    window.location.reload();
 }
 
-
+window.location.href = "https://wa.me/5531973434846";
