c2847e90ccbdd8ebc8897e528116573e889d3a60
laracamarino <laracamarino@gmail.com>
2022-04-04 04:03:23 -0300

    Versão 2 - Seleção de Itens Implementada

--- scripts.js
+++ scripts.js
@@ -1,4 +1,41 @@
-function() {
+function selecionarPrato(prato) {
+    console.log(prato);
+    
+    
+    const botaoSelecionado = document.querySelector(".borda-verde");
+    if (botaoSelecionado !== null) {
+        botaoSelecionado.classList.remove("borda-verde");
+    }
 
-
+    prato.classList.add("borda-verde");
 }
+function selecionarBebida(bebida) {
+    console.log(bebida);
+    
+    
+    const botaoSelecionado = document.querySelector(".bebidas").querySelector(".borda-verde");
+    if (botaoSelecionado !== null) {
+        botaoSelecionado.classList.remove("borda-verde");
+    }
+
+    bebida.classList.add("borda-verde");
+}
+function selecionarSobremesa(sobremesa) {
+    console.log(sobremesa);
+    
+    
+    const botaoSelecionado = document.querySelector(".sobremesas").querySelector(".borda-verde");
+    if (botaoSelecionado !== null) {
+        botaoSelecionado.classList.remove("borda-verde");
+    }
+
+    sobremesa.classList.add("borda-verde");
+}
+
+function fecharPedido() {
+
+
+    
+}
+
+
